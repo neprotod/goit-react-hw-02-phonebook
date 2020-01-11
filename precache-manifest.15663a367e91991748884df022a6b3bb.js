bcd96bba19c7c421ec3aca33f4aa8af22152ec20
Alexander Podlesnyi <gwebdzen@gmail.com>
2020-01-11 23:15:58 +0200

    Updates

---
+++ precache-manifest.15663a367e91991748884df022a6b3bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df9757f428859de2893d107f832ac99",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "594af85d3992b1a470ab",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.026e4396.chunk.css"
+  },
+  {
+    "revision": "c85e143e0c0e99bb36fc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.054f414a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.054f414a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "594af85d3992b1a470ab",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.65e692c6.chunk.js"
+  },
+  {
+    "revision": "612070f4d17ddaaf924f",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.8d03b19e.js"
+  }
+]);
